ef5a21eb47f53465ebc674846b4ba8797a15950c
Dirk Gütlin <34550289+DiGyt@users.noreply.github.com>
2022-10-25 22:21:29 +0200

    add reqs

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ from setuptools import setup
 setup(name='uneye',
       version='0.1',
       packages=['uneye'],
+      install_requires=["scikit-image",
+                        "torch",
+                        "numpy",
+                        "scipy",
+                        "scikit-learn",
+                        "matplotlib",
+                       ],
       include_package_data=True
 )
